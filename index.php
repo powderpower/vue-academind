264858c07e7ffdeb8d1576f8261eacd4cd83949d
Andrey Bogachevskiy <a.bogachevskiy@efko.ru>
2020-02-13 12:34:42 +0300

    step commit

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Дефолтный синтакс vue.js</title>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <script src="/js/vue.js"></script>
+    </head>
+    <body>
+    </body>
+</html>
