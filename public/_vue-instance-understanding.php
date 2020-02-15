06c853f4acaba29f996c09dcb6942440c6cda604
Admin Technostor <tehnostor.root@gmail.com>
2020-02-15 17:29:05 +0300

    vue instance understanding is comlited

---
+++ public/_vue-instance-understanding.php
@@ -0,0 +1,28 @@
+<script src="js/vue.js"></script>
+
+<!-- ref - vue.js key -->
+
+<div id="app-1">
+  <h1 ref="heading" v-html="title"></h1>
+  <button @click="show" ref="myButton">Show Paragraph</button>
+  <p v-if="showParagraph">This is not always visible</p>
+</div>
+
+<div id="app-2">
+  <h1 ref="heading">{{ title }}</h1>
+  <button @click="onChange">Change something in vueOne</button>
+  <!-- <hello></hello> -->
+  <!-- <hello></hello> -->
+</div>
+
+<div id="app-3"></div>
+
+<hr>
+
+<div id="app-4">
+    <h1>{{ title }}</h1>
+    <button @click="title = 'changed'">Update title</button>
+    <button @click="destroy">Destroy</button>
+</div>
+
+<script src="js/vue-instance-understanding/app.js"></script>
---
+++ public/js/vue-instance-understanding/app.js
@@ -0,0 +1,137 @@
+/**
+ * Для vue instance можно определить проперти из вне,
+ * но нельзя использовать их внутри instance,
+ * если они не определены внутри сразу,
+ * т.к. для них не создаются геттеры и сеттеры
+ */
+
+let data = {
+  title: 'The VueJS Instance',
+  showParagraph: false
+};
+
+/**
+ * переиспользуемый несколько раз компонент через соотв. тэг <hello></hello>
+ */
+/*
+Vue.component('hello', {
+  template: '<h1>Hello!</h1>',
+});
+*/
+
+const vmOne = new Vue({
+  data: data,
+  methods: {
+    show: function() {
+      this.showParagraph = true;
+      
+      this.updateTitle('The VueJS Instance (Updated)');
+      
+      console.log(this.$refs.myButton);
+      
+      this.$refs.myButton.innerText = 'test';
+    },
+    updateTitle: function(title) {
+      this.title = title;
+      console.log('Title updated');
+    }
+  },
+  computed: {
+    lowercaseTitle: function() {
+      return this.title.toLowerCase();
+    }
+  },
+  watch: {
+    title: function(value) {
+      console.log('Title changed, new value: ' + value);
+    }
+  }
+});
+
+vmOne.$mount('#app-1');
+
+setTimeout(function() {
+  vmOne.title = 'Changed by timer';
+  vmOne.show();
+}, 3e3);
+
+/**
+ * правила изменились, теперь это убивает интерполяцию {{ title }},
+ * поэтому нужно присваивать через v-html
+ */
+vmOne.$refs.heading.innerText = 'Something else';
+
+const vmTwo = new Vue({
+    el: '#app-2',
+    data: {
+      title: 'The second instance',
+    },
+    methods: {
+      onChange: function () {
+        vmOne.title = 'Changed!';
+      },
+    }
+});
+
+console.log(vmOne, vmOne.$data);
+
+const vmThree = new Vue({
+  // el: 'hello', // так заменяет один дом-елемент <hello></hello>
+  template: '<h1>Hello!</h1>',
+});
+
+// vmThree.$mount('#app-3');
+vmThree.$mount(); // тоже самое
+document.getElementById('app-3').appendChild(vmThree.$el);
+
+const vmFour = new Vue({
+  el: '#app-4',
+  data: {
+    title: 'The VueJs Instance',
+  },
+  beforeCreate: function() {
+    console.log('beforeCreate()')
+  },
+  created: function() {
+    console.log('created()');
+  },
+  beforeMount: function() {
+    console.log('beforeMount()')
+  },
+  mounted: function() {
+    console.log('mounted()');
+  },
+  beforeUpdate: function() {
+    console.log('beforeUpdate()');
+  },
+  updated: function() {
+    console.log('updated()');
+  },
+  beforeDestroy: function() {
+    console.log('beforeDestroy()');
+  },
+  destroyed: function() {
+    console.log('destroyed()');
+  },
+  methods: {
+    destroy: function() {
+        this.$destroy();
+    },
+  },
+})
+
+// vue instance lifecycle
+// beforeCreate - до того как instance фактически создан
+// created - instance создан, со всеми проперти и проч
+// beforeMount - instance готов к интеграции в дом, скомпилированы темплейты, элемент el - готов
+// updated - instance вставлен в дом
+// beforeUpdate - перед тем как вставить в дом, после того, как уже было событие updated
+// beforeDestroy - перед тем как убить instance
+// destroyed - instance убит
+
+/**
+ * virtual dom прослойка между Vue и DOM, который пропускает через себя изменения
+ * в instance, к примеру:
+ * updated не произойдет, если проперти не имеет реальных изменений.
+ * при вызове $destro(), виртуальный дом больше не существует и не контролирует дом.
+ */
