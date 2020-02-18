cad4960d384f25b0293b739f457a29ad7d50cd7a
Andrey Bogachevskiy <a.bogachevskiy@efko.ru>
2020-02-18 13:29:46 +0300

    step commit

--- vue-advanced/src/App.vue
+++ vue-advanced/src/App.vue
@@ -2,18 +2,25 @@
     <div class="container">
         <div class="row">
             <div class="col-xs-12">
-               
+               <app-qoute>
+                   <h2>The Quote</h2>
+                   <p>A wonderful Qoute</p>
+               </app-qoute>
             </div>
         </div>
     </div>
 </template>
 
 <script>
+    import Quote from './components/Quote.vue';
 
     export default {
-        
+        components: {
+            'app-qoute': Quote,
+        }
     }
 </script>
 
 <style>
+    /** к слотам не присваивается в родительском элементе */
 </style>
