6bf57cbea4e17810f4df10ee24bddef2f75ede3e
Andrey Bogachevskiy <a.bogachevskiy@efko.ru>
2020-02-20 08:34:17 +0300

    step commit

--- vue-advanced/src/App.vue
+++ vue-advanced/src/App.vue
@@ -2,11 +2,27 @@
     <div class="container">
         <div class="row">
             <div class="col-xs-12">
-               <app-qoute>
+               
+               <!-- Динамический компонент -->
+               <button @click="selectedComponent = 'app-quote'">Quote</button>
+               <button @click="selectedComponent = 'app-author'">Author</button>
+               <button @click="selectedComponent = 'app-new'">New</button>
+               <hr>
+               <p>{{ selectedComponent }}</p>
+               
+               <component :is="selectedComponent">
+                   <p>Default Content</p>
+               </component>
+               <!-- -->
+               
+               <!-- Обычный копонент -->
+               <app-quote>
                    <h2 slot="title">{{ quoteTitle }}</h2>
                    <p slot="content">A wonderful Qoute</p>
                    <p>Слот без имени</p>
-               </app-qoute>
+               </app-quote>
+               <!-- -->
+
             </div>
         </div>
     </div>
@@ -14,15 +30,20 @@
 
 <script>
     import Quote from './components/Quote.vue';
+    import Author from './components/Author.vue';
+    import New from './components/New.vue';
 
     export default {
         data: function () {
             return {
                 quoteTitle: 'The Quote',
+                selectedComponent: 'app-quote',
             };
         },
         components: {
-            'app-qoute': Quote,
+            'app-quote': Quote,
+            'app-author': Author,
+            'app-new': New,
         }
     }
 </script>
